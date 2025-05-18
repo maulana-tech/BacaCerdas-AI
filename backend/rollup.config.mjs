4c143769244fc69ae53a8bccf78691e2e1fd89f6
maulana-tech <firdaussyah03@gmail.com>
2025-05-18 16:06:18 +0700

    style(rollup): format config file for better readability
    Reformat the rollup.config.mjs file to improve code readability by aligning the indentation and organizing the external dependencies list

--- backend/rollup.config.mjs
+++ backend/rollup.config.mjs
@@ -2,17 +2,24 @@ import esbuild from "rollup-plugin-esbuild";
 
 /** @type {import('rollup').RollupOptions} */
 export default {
-    input: 'src/index.ts',
-    output: {
-        file: 'dist/index.js',
-        format: 'cjs',
-        sourcemap: true,
-        banner: '#!/usr/bin/env node',
-    },
-    plugins: [
-        esbuild({
-            minify: true,
-        })
-    ],
-    external: ["dotenv/config", "express", "cors", "helmet", "winston", "winston-daily-rotate-file"],
-}
+  input: "src/index.ts",
+  output: {
+    file: "dist/index.js",
+    format: "cjs",
+    sourcemap: true,
+    banner: "#!/usr/bin/env node",
+  },
+  plugins: [
+    esbuild({
+      minify: true,
+    }),
+  ],
+  external: [
+    "dotenv/config",
+    "express",
+    "cors",
+    "helmet",
+    "winston",
+    "winston-daily-rotate-file",
+  ],
+};
