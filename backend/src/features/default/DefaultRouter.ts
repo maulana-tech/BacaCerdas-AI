006bebade0c527f233f7481f2fb2f1dd9fb5087e
fwwz-id <fwwz.id@gmail.com>
2025-05-18 18:27:22 +0700

    feat/default: implement contract for router

--- backend/src/features/default/DefaultRouter.ts
+++ backend/src/features/default/DefaultRouter.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
+import type { APIRouter } from "../../lib/types/Router";
 import DefaultController from "./DefaultController";
 
-export default class DefaultRouter {
+export default class DefaultRouter implements APIRouter {
   get routes() {
     return Router().get("/", new DefaultController().index);
   }
