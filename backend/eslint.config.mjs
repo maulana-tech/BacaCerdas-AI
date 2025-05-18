f76dd6086392a2ed72e677b599ff57414b090244
fwwz-id <fwwz.id@gmail.com>
2025-05-18 18:20:07 +0700

    chore: omit dist from lint check

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -6,4 +6,12 @@ import tseslint from "typescript-eslint";
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  {
+    ignores: [
+      "**/dist/**",
+      "**/node_modules/**",
+      "**/coverage/**",
+      "**/build/**"
+    ]
+  }
 );
