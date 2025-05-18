44bab62694518e10a06b8b7bca1dc7abdb265cd1
maulana-tech <firdaussyah03@gmail.com>
2025-05-18 23:51:17 +0700

    style(rollup): reformat configuration file for consistency
    Refactor the rollup configuration file to improve readability by aligning indentation and consolidating the external dependencies array. This change does not affect the functionality of the build process.

--- backend/rollup.config.mjs
+++ backend/rollup.config.mjs
@@ -2,24 +2,17 @@ import esbuild from "rollup-plugin-esbuild";
 
 /** @type {import('rollup').RollupOptions} */
 export default {
-  input: "src/index.ts",
+  input: 'src/index.ts',
   output: {
-    file: "dist/index.js",
-    format: "cjs",
-    sourcemap: true,
-    banner: "#!/usr/bin/env node",
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true,
+      banner: '#!/usr/bin/env node',
   },
   plugins: [
-    esbuild({
-      minify: true,
-    }),
+      esbuild({
+          minify: true,
+      })
   ],
-  external: [
-    "dotenv/config",
-    "express",
-    "cors",
-    "helmet",
-    "winston",
-    "winston-daily-rotate-file",
-  ],
+  external: ["dotenv/config", "express", "cors", "helmet", "winston", "winston-daily-rotate-file"],
 };
